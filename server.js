87dcf451f2bdd40b23d08f7a886bf45ae7f2baa3
Glitch (chambray-magnificent-teeth) <none>
2020-04-23 07:28:36 +0000

    B6

--- server.js
+++ server.js
@@ -6,17 +6,27 @@
 const express = require("express");
 const app = express();
 
+// lowdb
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ todos: ["Học code tại CodersX"] }).write();
+// lowdb-end
+
 app.set("view engines", "pug");
 app.set("views", "./views");
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-const todoList = ["Đi chợ", "Nấu cơm", "Rửa bát", "Học code tại CodersX"];
-
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
   res.render("index.pug");
 });
+
+const todoList = db.get("todos").value();
 
 app.get("/todos", (req, res) => {
   res.render("./todos/index.pug", {
@@ -26,9 +36,7 @@ app.get("/todos", (req, res) => {
 
 app.get("/todos/search", (req, res) => {
   const qValue = req.query.q.toLowerCase();
-  const newTodoList = todoList.filter(item =>
-    item.toLowerCase().includes(qValue)
-  );
+  const newTodoList = todoList.filter(item => item.toLowerCase().includes(qValue));
   res.render("./todos/search.pug", {
     newTodoList
   });
@@ -36,7 +44,9 @@ app.get("/todos/search", (req, res) => {
 
 app.post("/todos/create", (req, res) => {
   const value = req.body.todo;
-  todoList.push(value);
+  db.get("todos")
+    .push(value)
+    .write();
   res.redirect("/todos");
 });
 

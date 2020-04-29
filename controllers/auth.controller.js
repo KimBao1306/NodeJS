cb07915cedf9639c4eace3989e8d28f3f98ee493
Glitch (b16-2) <none>
2020-04-29 10:39:13 +0000

    B16-2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,13 @@
+const bcrypt = require("bcrypt");
 const md5 = require("md5");
-
 const db = require("../db.js");
 
 module.exports.login = (req, res) => {
   res.render("./auth/login.pug");
 };
 
-module.exports.loginPost = (req, res) => {
+module.exports.loginPost = async (req, res) => {
   const emailUser = req.body.email;
-  const passwordUser = md5(req.body.password);
 
   const user = db
     .get("users")
@@ -23,7 +22,24 @@ module.exports.loginPost = (req, res) => {
     return;
   }
 
-  if (passwordUser !== user.password) {
+  if (user.wrongLoginCount >= 3) {
+    res.render("./auth/login.pug", {
+      errors: [
+        "Bạn đã vượt quá số lần đăng nhập. Hãy sử dụng chức năng tìm mật khẩu nhé."
+      ],
+      values: req.body
+    });
+    return;
+  }
+
+  const checkPass = await bcrypt.compare(req.body.password, user.password);
+
+  if (!checkPass) {
+    db.get("users")
+      .find({ id: user.id })
+      .assign({ wrongLoginCount: ++user.wrongLoginCount })
+      .write();
+
     res.render("./auth/login.pug", {
       errors: ["Your email or password were wrong. Please try again"],
       values: req.body
@@ -32,6 +48,12 @@ module.exports.loginPost = (req, res) => {
   }
 
   res.cookie("idUser", user.id);
+
+  res.redirect("/");
+};
+
+module.exports.logout = (req, res) => {
+  res.clearCookie("idUser");
 
   res.redirect("/");
 };
